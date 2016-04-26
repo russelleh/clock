f4effd74128821c12462ae28086d79f548b884f7
russelleh <russell.west.eh@gmail.com>
2016-04-26 12:46:48 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function rotate() {
+  var timestamp  = Date.now() / 1000;
+  var daystamp   = timestamp % 86400;
+  var proportion = daystamp / 86400;
+  var proportion = proportion;
+  var angle      = proportion * 360;
+  $("#back").css({ transform: "rotate(" + angle + "deg)"});
+};
+
+$(document).ready(function() {
+  rotate();
+  setInterval(function() {
+    rotate();
+  }, 1000)
+});
