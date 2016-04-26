22ea6a7bf34064991df45331311fc00797c5cf84
russelleh <russell.west.eh@gmail.com>
2016-04-26 14:15:24 -0400

    Delete unnecessary line

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function rotate() {
   var timestamp  = Date.now() / 1000;
   var daystamp   = timestamp % 86400;
   var proportion = daystamp / 86400;
-  var proportion = proportion;
   var angle      = proportion * 360;
   $("#back").css({ transform: "rotate(" + angle + "deg)"});
 };
